7b165594fabfae43296376d5314bf82d2dd4a98c
tehp <maccraig98@gmail.com>
2024-07-18 22:59:10 -0700

    Test providing base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), { provide: APP_BASE_HREF, useValue: '/i2idigital' }]
 };
