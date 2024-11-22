5476f070ada82cbb79c3465950bf60b3ae113c27
tehp <maccraig98@gmail.com>
2024-11-22 00:12:43 -0800

    Add password, added scroll to campaigns

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from '../background/background.component';
 import { NavBackComponent } from '../nav-back/nav-back.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [BackgroundComponent, NavBackComponent],
+  imports: [BackgroundComponent, RouterLink, NavBackComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  showContent = false;
 
+  public processPass(event: any) {
+    if (event.target.value === "tehp") {
+      this.showContent = true;
+    }
+  }
 }
