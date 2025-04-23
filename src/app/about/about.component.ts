6ae7bf408afd6f3984840907a450b114f219a0fb
tehp <maccraig98@gmail.com>
2025-04-22 21:53:08 -0700

    Add about page number animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackgroundComponent } from '../background/background.component';
 import { NavBackComponent } from '../nav-back/nav-back.component';
+import { CountUp } from 'countup.js';
 
 @Component({
   selector: 'app-about',
@@ -9,6 +10,13 @@ import { NavBackComponent } from '../nav-back/nav-back.component';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit {
+  ngOnInit(): void {
+    [
+      new CountUp('streams-stat', 500, { duration: 5, suffix: 'M+' }),
+      new CountUp('fans-stat', 2, { duration: 5, suffix: 'B+', decimalPlaces: 1 }),
+      new CountUp('cpm-stat', 0.94, { duration: 5, prefix: '$', decimalPlaces: 2, startVal: 3 }),
+      new CountUp('follower-stat', 23, { duration: 5, suffix: 'M+' })
+    ].forEach(c => c.start())
+  }
 }
