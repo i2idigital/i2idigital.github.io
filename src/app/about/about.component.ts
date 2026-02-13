adc0e34e528fa48bc01cb852dbafdec8eb7f16b9
tehp <maccraig98@gmail.com>
2026-02-12 21:31:34 -0800

    Update team, mobile bugfixes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,9 +13,9 @@ import { CountUp } from 'countup.js';
 export class AboutComponent implements OnInit {
   ngOnInit(): void {
     [
-      new CountUp('streams-stat', 500, { duration: 5, suffix: 'M+' }),
-      new CountUp('fans-stat', 2, { duration: 5, suffix: 'B+', decimalPlaces: 1 }),
-      new CountUp('cpm-stat', 0.46, { duration: 5, prefix: '$', decimalPlaces: 2, startVal: 3 }),
+      new CountUp('streams-stat', 750, { duration: 5, suffix: 'M+' }),
+      new CountUp('fans-stat', 5, { duration: 5, suffix: 'B+', decimalPlaces: 1 }),
+      new CountUp('cpm-stat', 0.38, { duration: 5, prefix: '$', decimalPlaces: 2, startVal: 3 }),
       new CountUp('follower-stat', 23, { duration: 5, suffix: 'M+' })
     ].forEach(c => c.start())
   }
